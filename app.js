fcf06fc33cca5fb187e4a4fdecc37fd13ee6aa3e
Danny <deo.dny@gmail.com>
2025-02-06 23:51:37 -0500

    muestra el nombre del amigo añadido

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 let listaDeAmigos = [];
-
+let i=0
 
 function agregarAmigo() {
-    
+
     let nombreDeAmigo = document.getElementById("amigo").value;
 
     if (/^[A-Za-z]+$/.test(nombreDeAmigo)){
         listaDeAmigos.push(nombreDeAmigo);
+        asignarTextoElemento ('ul',`${listaDeAmigos[i]}`)
+        i++;
+
 
     } else if (/^([0-9])*$/.test(nombreDeAmigo)){
-        alert('Ingrese un dato valido')
+        alert('Ingrese un dato valido') 
     }
-   
+    
     console.log(nombreDeAmigo);
     console.log(listaDeAmigos);
     
@@ -19,6 +22,11 @@ function agregarAmigo() {
     return nombreDeAmigo;
 }
 
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return; 
+}
 
 function limpiarTexto(){
     document.querySelector('#amigo').value = '';
