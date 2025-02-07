5eb824120d6a3825dec508f0d2a4ff75e0063bcb
Danny <deo.dny@gmail.com>
2025-02-06 22:07:23 -0500

    crea funciones de ingreso de amigo

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-// Prueba 1
-// Prueba 2
-// Prueba 3
-// Prueba 4
-// Prueba 5
+let listaDeAmigos = [];
+
+
+function agregarAmigo() {
+    
+    let nombreDeAmigo = document.getElementById("amigo").value;
+
+    if (/^[A-Za-z]+$/.test(nombreDeAmigo)){
+        listaDeAmigos.push(nombreDeAmigo);
+
+    } else if (/^([0-9])*$/.test(nombreDeAmigo)){
+        alert('Ingrese un dato valido')
+    }
+   
+    console.log(nombreDeAmigo);
+    console.log(listaDeAmigos);
+    
+    limpiarTexto();
+    return nombreDeAmigo;
+}
+
+
+function limpiarTexto(){
+    document.querySelector('#amigo').value = '';
+}
