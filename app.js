49f04e03c5464b904791f2210ad98b9e01730874
Danny <deo.dny@gmail.com>
2025-02-10 23:54:58 -0500

    muestra nombre uno debajo de otro

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 let listaDeAmigos = [];
-let i=0
+let numeroMaximo = listaDeAmigos.length;
 
 function agregarAmigo() {
 
     let nombreDeAmigo = document.getElementById("amigo").value;
 
-    if (/^[A-Za-z]+$/.test(nombreDeAmigo)){
+    if (/^[A-Za-z]*$/.test(nombreDeAmigo)){
         listaDeAmigos.push(nombreDeAmigo);
-        asignarTextoElemento ('ul',`${listaDeAmigos[i]}`)
-        i++;
-
-
-    } else if (/^([0-9])*$/.test(nombreDeAmigo)){
+        asignarTextoElemento ('ul',`${listaDeAmigos.join('<br>')}`)
+        
+    } else if (/^([0-9])*$/.test(nombreDeAmigo)){   
         alert('Ingrese un dato valido') 
     }
     
     console.log(nombreDeAmigo);
     console.log(listaDeAmigos);
-    
+
     limpiarTexto();
     return nombreDeAmigo;
 }
 
+
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
+    elementoHTML.innerHTML = texto
     return; 
 }
 
