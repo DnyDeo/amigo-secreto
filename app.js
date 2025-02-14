141baf06fb654a99babd1ce503baafa893c57d3b
Danny <deo.dny@gmail.com>
2025-02-13 22:19:38 -0500

    Muestra dato en funcion de numero aleatorio

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
 let listaDeAmigos = [];
-let listaNumerosSorteado = [];
-let numeroMaximo = 10;
 
 function agregarAmigo() {
-
+    // Valida el dato, agrega a la matrix y muestra en forma de lista
     let nombreDeAmigo = document.getElementById("amigo").value;
-
-    if (/^[A-Za-z]*$/.test(nombreDeAmigo)){
+    if (/^[A-Za-z]+$/.test(nombreDeAmigo)){
         listaDeAmigos.push(nombreDeAmigo);
         asignarTextoElemento ('listaAmigos',`${listaDeAmigos.join('<br>')}`)
-        
-    } else if (/^([0-9])*$/.test(nombreDeAmigo)){   
+    } 
+    // Valida el dato y alerta la necesidad de un dato valido
+    else if (/^([0-9])*$/.test(nombreDeAmigo)){   
         alert('Ingrese un dato valido') 
     }
-    
-    console.log(nombreDeAmigo);
-    console.log(listaDeAmigos);
 
+    // Limpia la caja y se prepara para un proximo dato
     limpiarTexto();
     return nombreDeAmigo;
 }
 
 function generarAmigoSecreto() {
+    // Conoce la cantidad de datos insertados
+    let numeroMaximo = listaDeAmigos.length;
+    // Asigna un numero aletorio en funcion de la cantidad de datos ingresados
     let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
-    console.log(numeroGenerado);
-    asignarTextoElemento ('resultado',`${listaDeAmigos[numeroGenerado]}`)
+    // Resta 1 para no enviar un dato superior a la matrix (0,1,2,3...) y se tiene en cuenta el dato 0
+    let numeroGeneradoIncluidoElCero =numeroGenerado-1;
+    // Evalua la cantidad de datos ingresados para realizar un sorteo justo
+    if (numeroMaximo <=1){
+        alert('Por favor ingrese al menos dos nombres') 
+    }
+    // Sortea el dato acorde al numero generado
+    else {
+    asignarTextoElemento ('resultado',`El amigo secreto sorteado es: ${listaDeAmigos[numeroGeneradoIncluidoElCero]}`)
+    // Reinicia condiciones y se prepara para el proximo sorteo
+    condicionesIniciales();
+    return numeroMaximo; 
+    }
 }
 
-generarAmigoSecreto();
-
+// Funcion de asignacion de texto
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.getElementById(elemento);
     elementoHTML.innerHTML = texto
     return; 
 }
 
+// Funcion de limpieza de texto
 function limpiarTexto(){
-    document.querySelector('#amigo').value = '';
+    document.getElementById('amigo').value = '';
+    asignarTextoElemento ('resultado','');   
+}
+
+// Funcion de reset
+function condicionesIniciales(){
+    asignarTextoElemento ('listaAmigos','');
+    document.getElementById('amigo').value = '';
+    listaDeAmigos = [];
 }
